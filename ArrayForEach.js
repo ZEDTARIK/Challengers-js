abe71e9e27e0bacf6effa3992fffb1faa2909868
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-04-14 10:34:38 +0000

    Object and Set function , reduce

--- ArrayForEach.js
+++ ArrayForEach.js
@@ -13,8 +13,59 @@ const scoreWord = score.map(point => {
     return point > 50 ? "higer than 50" : "Lower than 50";
 });
 
-console.log(scoreWord);
+// console.log(scoreWord);
 
 const numbers = [9.23, 10.58, 41.12];
 const integers = numbers.map(number => Math.round(number));
-console.log(integers);
+// console.log(integers);
+
+
+// array filter
+const tasks = [
+    { task: "Learn JS", done: true },
+    { task: "Learn Laravel", done: false },
+    { task: "Learn Angular", done: false }
+];
+
+const completedTask = tasks.filter(task => task.done);
+//console.table(completedTask);
+
+
+const members = [
+    {
+        name: "Elon Musk",
+        companies: ["Telsa", "SpaceX", "StarLink"]
+    },
+    {
+        name: "Mark Zuckerberg",
+        companies: ["facebook", "whatSup", "Instagram"]
+    },
+    {
+        name: "Jack Dorsey",
+        companies: ["Twitter", "Square"]
+    }
+];
+
+const membersWithCompany = members.reduce((acc, curr) => {
+    return acc.concat(curr.companies);
+}, []);
+
+console.table(membersWithCompany);
+
+
+const posts  = [
+    {
+        title: "Post 1",
+        lang: ["English", "French", "German"]
+    },
+    {
+        title: "Post 2",
+        lang: ["English", "French", "German", "Spanish"]
+    }
+];
+ // eliminate double languages
+const translatedLanguages =  [... new Set(posts.map((post) => post.lang).reduce((acc, post) => acc.concat(post)))];
+
+console.table(translatedLanguages);
+
+
