3aa4e0bacafc5d38be6e7cb036d2fa416bbd5134
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-05-16 16:55:51 +0100

    add comment the sum of array

--- SumWithOutTheHeighestAndLowerNumber.js
+++ SumWithOutTheHeighestAndLowerNumber.js
@@ -1,3 +1,11 @@
+/**
+ * The function returns the sum of an array after removing the highest and lowest numbers.
+ * @param arr - an array of numbers for which we want to find the sum of all the numbers except the
+ * highest and lowest numbers.
+ * @returns The function `SumWithOutTheHeighestAndLowerNumber` is returning the sum of all the numbers
+ * in the input array `arr` except for the highest and lowest numbers. If the input array is `null`, it
+ * returns `0`.
+ */
 function SumWithOutTheHeighestAndLowerNumber(arr) {
     return arr == null ? 0 : arr.sort((a,b) => a-b).slice(1, -1).reduce((acc, cum) => acc + cum, 0);
 }
